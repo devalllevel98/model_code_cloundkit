0499afd1411d5aa89629d32f5ca36ad588577ec4
roxie <roxie@roxies-MacBook-Pro.local>
2024-01-08 15:36:09 +0700

    up

---
+++ animation_balloon/color.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color offWhiteColor = Color.fromRGBO(250, 250, 250, 1);
+  static const Color greyLightColor = Color(0xFFE0E0E0);
+  static const Color whiteColor = Colors.white;
+
+  static Color bubbleRedColor = Colors.red[400]!;
+  static Color bubbleYellowColor = Colors.yellow[400]!;
+ 
+  static Color blackColor = Colors.grey[900]!;
+
+  static Color blueColor = Colors.lightBlue[200]!;
+  static Color blueLightColor = Colors.lightBlue[100]!;
+  static const Color brownColor = Color.fromRGBO(192, 176, 90, 1);
+
+
+  static const Color greenColor = Color.fromRGBO(109, 180, 134, 0.8);
+  static Color beigeColor = const Color.fromRGBO(217, 202, 179, 0.8);
+  static Color redColor = const Color.fromRGBO(255, 114, 114, 1);
+  static Color redColorLight = const Color.fromRGBO(255, 181, 181, 1);
+  static const Color greenLightColor = Color.fromRGBO(109, 180, 134, 1);
+  static Color beigeLightColor = const Color.fromRGBO(217, 210, 179, 1);
+
+}
--- menu/duppleAnimation.dart
+++ menu/duppleAnimation.dart
@@ -1,30 +1,53 @@
 // ignore_for_file: prefer_const_constructors, use_build_context_synchronously
 
 import 'dart:io';
-
 import 'package:flutter/material.dart';
-
-import '../game.dart';
 import 'package:action_slider/action_slider.dart';
+import 'package:animated_background/animated_background.dart';
+import 'package:flutter/material.dart';
+import 'package:tetramind/board.dart';
+import 'package:tetramind/gui.dart';
 
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+
+
+class HomePage extends StatefulWidget {
+  //  const HomePage({super.key});
+  @override
+  State<HomePage> createState() => _HomePage();
+}
+class _HomePage extends State<HomePage> with TickerProviderStateMixin {
+  get score => null;
 
   @override
   Widget build(BuildContext context) {
-    double height = MediaQuery.of(context).size.height;
+        double height = MediaQuery.of(context).size.height;
     double width = MediaQuery.of(context).size.width;
     return Scaffold(
-        body: Container(
+      backgroundColor: Colors.transparent,
+      body: AnimatedBackground(
+        behaviour: RandomParticleBehaviour(
+          options: const ParticleOptions(
+            spawnMaxRadius: 50,
+            spawnMinSpeed: 10.00,
+            particleCount: 68,
+            spawnMaxSpeed: 50,
+            minOpacity: 0.3,
+            spawnOpacity: 0.4,
+            baseColor:  Colors.blue,
+            // image: Image(image: AssetImage('assets/anilogo.png')),
+          ),
+        ),
+        vsync: this,
+        child:  Container(
       width: width,
       height: height,
-      decoration: BoxDecoration(
-        color: Colors.black,
-        image: DecorationImage(
-          image: AssetImage("assets/02.jpeg"),//anhr nen
-          fit: BoxFit.fitHeight,
-        ),
-      ),
+      // decoration: BoxDecoration(
+      //   color: Colors.black,
+      //   image: DecorationImage(
+      //     image: AssetImage("assets/02.jpeg"),
+      //     fit: BoxFit.fitHeight,
+      //   ),
+      // ),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.center,
         mainAxisAlignment: MainAxisAlignment.center,
@@ -32,11 +55,12 @@ class HomePage extends StatelessWidget {
           // Spacer(),
           Center(
                 child: Column(
+                  
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
                       ActionSlider.standard(
                         width: width * 0.6,
-                        backgroundColor: Color.fromARGB(255, 13, 32, 236),
+                        backgroundColor: Colors.transparent,
                         toggleColor: Color(0xffffffff),
                         icon: Icon(
                           Icons.play_arrow_outlined,
@@ -56,11 +80,11 @@ class HomePage extends StatelessWidget {
                         ),
                         action: (controller) async {
                           controller.loading();
-                          await Future.delayed(const Duration(seconds: 3));
+                          await Future.delayed(const Duration(seconds: 2));
                           Navigator.pushAndRemoveUntil(
                             context,
                             MaterialPageRoute(
-                                builder: (BuildContext context) => GamePage()),
+                                builder: (BuildContext context) => const Board()),
                             (Route<dynamic> route) => false,
                           );
                           controller.success();
@@ -69,7 +93,7 @@ class HomePage extends StatelessWidget {
                     SizedBox(height: 30.0),
                     ActionSlider.standard(
                         width: width * 0.6,
-                        backgroundColor: Color.fromARGB(255, 200, 168, 247),
+                        backgroundColor: Colors.transparent,
                         toggleColor: Color(0xffffffff),
                         icon: Icon(
                           Icons.gite_outlined,
@@ -89,11 +113,11 @@ class HomePage extends StatelessWidget {
                         ),
                         action: (controller) async {
                           controller.loading();
-                          await Future.delayed(const Duration(seconds: 3));
+                          await Future.delayed(const Duration(seconds: 2));
                           Navigator.pushAndRemoveUntil(
                             context,
                             MaterialPageRoute(
-                                builder: (BuildContext context) => GamePage()),
+                                builder: (BuildContext context) => Guiline()),
                             (Route<dynamic> route) => false,
                           );
                           controller.success();
@@ -102,7 +126,7 @@ class HomePage extends StatelessWidget {
                   SizedBox(height: 30.0),
                                         ActionSlider.standard(
                         width: width * 0.6,
-                        backgroundColor: Color.fromARGB(255, 224, 10, 53),
+                        backgroundColor: Colors.transparent,
                         toggleColor: Color(0xffffffff),
                         icon: Icon(
                           Icons.exit_to_app_outlined,
@@ -122,7 +146,7 @@ class HomePage extends StatelessWidget {
                         ),
                         action: (controller) async {
                           controller.loading();
-                          await Future.delayed(const Duration(seconds: 3));
+                          await Future.delayed(const Duration(seconds: 2));
                           exit(0);
                           // controller.success();
                         },
@@ -136,7 +160,12 @@ class HomePage extends StatelessWidget {
 
         ],
       ),
-    ));
+    )
+      ),
+    );
   }
 }
-//action_slider: ^0.6.1
+
+
+
+
